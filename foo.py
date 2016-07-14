81f4d666e87820f27a5ef8e0b36e3e2baa0f1bcc
Samson Yeung <samson.yeung@oracle.com>
2016-07-13 17:06:44 -0700

    latest foo test script.

--- foo.py
+++ foo.py
@@ -1,36 +1,12 @@
 import time
-import sys
 
 import lighthouse
 
 l = lighthouse.Lighthouse()
-#l.foo()
-#time.sleep(5)
-#l.bar()
-#l.dmx.disconnect()
-#sys.exit(0)
-
-print 'hi gerald'
-l.dmx.setChannel(5, 255)
-l.dmx.render()
-time.sleep(10)
-l.dmx.setChannel(5, 0)
-l.dmx.render()
-l.dmx.disconnect()
-sys.exit(0)
-
-
-
+l.lamp(10)
+time.sleep(5)
 l.lamp(100)
-print 'lamp on'
-#l.foo()
-time.sleep(5)
+time.sleep(3)
+l.lamp(50)
 l.set_pan_position(0)
-time.sleep(5)
-l.set_pan_position(90)
-#time.sleep(15)
-time.sleep(5)
-l.lamp(0)
-print 'lamp off'
 l.dmx.disconnect()
-#l.bar()
