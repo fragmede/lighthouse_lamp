660f811ba00cf2072c651f861552fc2b56adf2a6
Samson Yeung <samson.yeung@oracle.com>
2016-07-13 17:03:26 -0700

    util: cast dmx return values to int

--- util.py
+++ util.py
@@ -8,8 +8,8 @@ def get_default_port():
         return '/dev/tty.usbserial-ENT095626'
 
 def percent_to_dmx(int_percent):
-    return int_percent/100 * 255
+    return int(int_percent/100 * 255)
 
 def degrees_to_dmx(degrees, min_range=0, max_range=360):
-    return degrees/(max_range-min_range) * 255
+    return int(degrees/(max_range-min_range) * 255)
 
